3575636cbd0e8c398cabc101bd55921eb5eabc8e
a-jartseva <33483569+a-jartseva@users.noreply.github.com>
2022-12-30 00:34:29 +0000

    Implement tilt correction
    Tilt correction, including:
    - Switching from a single calib file to a folder.
    - Fitting a 2d linear model (plane) to (deviation of angle from average angle across all beads)~(xy position of the bead) (both xy position and angle deviation are averaged between the different z-positions for each bead) -> applying this model to the calibration data itself.
    - Polynomial fitting of dx, dy, dz, dd and dr vs angle, using data points from all the calibs.
    --- For dx and dy, instead of absolute xy position calculated the xy shift from the average xy coordinate for each bead; to avoid bias due to average xy calculation, for this only using beads that appear in >95% frames.

--- calibAngle.m
+++ calibAngle.m
@@ -2,7 +2,7 @@
 % It takes the calibration Peak Fit result file as the first argument, the step in z between consecutive frames as its second obligatory argument. Optional arguments include the column numbers in the file, corresponding to [x y intensity frame]; and the range in which to fit the data, either wrt angle/pitch of the double helix or the z coordinate. The optional arguments have defaults. Default column numbers correspond to the current formatting of Peak Fit results.
 % The outputs are polynomial functions, describing the dependency of the z coordinate, the distance between the dots and the ratio of their intensities on the angle, as well as the dependency of the xy coordinates on the position in z. The final output value is the angular range of this particular calibration.
 
-function [dx dy dz dd dr aRange] = calib( fileN,sepCalib,step,varargin)
+function [dx dy dz dd dr aRange model] = calib( fileN,sepCalib,step,varargin)
 params = struct('cols', [10 11 9 1], 'rangeToFit', [600 3800], 'ZorAngleorFrame', 'Z');
 paramNames = fieldnames(params);
 
@@ -24,6 +24,7 @@ cols = params.cols;
 rangeToFit = params.rangeToFit;
 ZorAngleorFrame = params.ZorAngleorFrame;
 
+beadID = [];
 x1 = [];
 x2 = [];
 y1 = [];
@@ -35,59 +36,99 @@ fAll = [];
 ratAll = [];
 distAll = [];
 
-data = dlmread(fileN,sepCalib,9,0);
-
-%% For each frame, find the two lobes, extract their xy coordinates, the distance and the angle between them, their intensities and the ratio of the intensities %%
-for i = 1:max(data(:,cols(4))) %iteration through frames
-    if(i/100==floor(i/100))
-        i %display progress
-    end
+%Iterate over multiple calibration files
+for j = 1:numel(fileN)
+    data = dlmread(fileN{j},sepCalib,9,0);
+    %% For each frame, find the two lobes, extract their xy coordinates, the distance and the angle between them, their intensities and the ratio of the intensities %%
+    for i = 1:max(data(:,cols(4))) %iteration through frames
+        if(i/100==floor(i/100))
+            i %display progress
+        end
     
-    poses = find(data(:,cols(4))==i);
-    xs = data(poses,cols(1));
-    is = data(poses,cols(3));
-    fs = data(poses,cols(4));
-    if isempty(xs)
-        continue
+        poses = find(data(:,cols(4))==i);
+        xs = data(poses,cols(1));
+        is = data(poses,cols(3));
+        fs = data(poses,cols(4));
+        if isempty(xs)
+            disp(['Warning: no localisations found in frame ', ...
+                num2str(i), ', skipping'])
+            continue
+        end
+        if length(xs) == 1
+            disp(['Warning: only 1 localisation found in frame ', ...
+                num2str(i), ', skipping'])
+            continue
+        end
+        ys = data(poses,cols(2));
+    
+        D = squareform(pdist([xs ys],'euclidean')); %find distances     between all localisations in frame i
+        D(D>8)=0; %if localisations are too far or too close,     discard this pair (i.e. set distance to 0)
+        D(D<3)=0;
+        D=triu(D);
+    
+        [I,J] = ind2sub(size(D),find(D)); %indices of all non-zero distances, should be just a single distance if there are no spurious localisations
+        if length(I) > 1 %if >1 distance, skip this frame with a warning
+            disp(['Warning: >2 localisations found in frame ', ...
+                num2str(i), ', skipping'])
+            continue
+        end
+        if length(I) == 0 %if distance too large/small, skip this frame with a warning
+            disp(['Warning: localisations found in frame ', ...
+                num2str(i), ' are too far/close, skipping'])
+            continue
+        end
+    
+        x1s = xs(I);
+        x2s = xs(J);
+        y1s = ys(I);
+        y2s = ys(J);
+        dists = D(I,J);
+        intensity = mean([is(I) is(J)],2);
+        ratio = max([is(I) is(J)])./min([is(I) is(J)]);
+        angles = atan2(ys(I)-ys(J),xs(I)-xs(J));
+        angles(angles<0)=angles(angles<0)+pi;
+    
+        x1 = [x1;x1s];
+        x2 = [x2;x2s];
+        y1 = [y1;y1s];
+        y2 = [y2;y2s];
+        aAll = [aAll;angles];
+        iAll = [iAll;intensity];
+        fAll = [fAll;fs(1)*ones(length(x1s),1)];
+        ratAll = [ratAll;ratio];
+        distAll = [distAll;dists];
+
+        beadID = [beadID;j*ones(length(x1s),1)];
     end
-    ys = data(poses,cols(2));
-
-    D = squareform(pdist([xs ys],'euclidean')); %find distances between all localisations in frame i
-    D(D>8)=0; %if localisations are too far or too close, discard this pair (i.e. set distance to 0)
-    D(D<3)=0;
-    D=triu(D);
-
-    [I,J] = ind2sub(size(D),find(D)); %indices of all non-zero distances, should be just a single distance if there are no spurious localisations
-    if length(I) > 1 %if >1 distance, skip this frame with a warning
-        disp(['Warning: >2 localisations found in frame ', ...
-            num2str(i), ', skipping'])
-        continue
-    end
-
-    x1s = xs(I);
-    x2s = xs(J);
-    y1s = ys(I);
-    y2s = ys(J);
-    dists = D(I,J);
-    intensity = mean([is(I) is(J)],2);
-    ratio = max([is(I) is(J)])./min([is(I) is(J)]);
-    angles = atan2(ys(I)-ys(J),xs(I)-xs(J));
-    angles(angles<0)=angles(angles<0)+pi;
-
-    x1 = [x1;x1s];
-    x2 = [x2;x2s];
-    y1 = [y1;y1s];
-    y2 = [y2;y2s];
-    aAll = [aAll;angles];
-    iAll = [iAll;intensity];
-    fAll = [fAll;fs(1)*ones(length(x1s),1)];
-    ratAll = [ratAll;ratio];
-    distAll = [distAll;dists]; 
 end
 
 xAll = mean([x1 x2],2);
 yAll = mean([y1 y2],2);
 
+%% Correcting the tilt %%
+%For each bead at each frame, find the relative shift of angle from the mean angle of all beads
+aRel = [];
+for i = 1:max(fAll)
+    poses = find(fAll==i);
+    aRel(poses) = aAll(poses)-mean(aAll(poses));
+end
+%For each bead, find the average (over frames) relative shift wrt mean
+aAvg = [];
+xAvg = [];
+yAvg = [];
+for j = 1:max(beadID)
+    poses = find(beadID==j);
+    aAvg = [aAvg; mean(aRel(poses))];
+    xAvg = [xAvg; mean(xAll(poses))];
+    yAvg = [yAvg; mean(yAll(poses))];
+end
+
+%Fit linear model, return it later to apply to data
+model = fitlm([xAvg yAvg], aAvg);
+%Correct the angles
+aAll = aAll - predict(model, [xAll yAll]);
+
+%% Exclude the frames outside the specified range %%
 if ZorAngleorFrame == 'Z'
     idx = find((fAll*step >= rangeToFit(1)) & (fAll*step <= rangeToFit(2)));
 elseif ZorAngleorFrame == 'Angle'
@@ -101,6 +142,7 @@ fAll = fAll(idx);
 aAll = aAll(idx);
 distAll = distAll(idx);
 ratAll = ratAll(idx);
+beadID = beadID(idx);
 
 
 disp(aAll)
@@ -109,8 +151,24 @@ disp(length(aAll))
 aRange = [min(aAll) max(aAll)];
 
 %% Fit polynomial functions to... %%
-dx = polyfit(fAll*step,xAll,5); % ... x vs z, defined as frame*z-step
-dy = polyfit(fAll*step,yAll,5); % ... y vs z
+%First, obtain normalised x and y coordinates of each bead, which has >95% observations
+xAllNorm = [];
+yAllNorm = [];
+fAllNorm = [];
+numObs = accumarray(beadID, beadID, [], @length);
+for j = 1:max(beadID)
+    poses = find(beadID==j);
+    aAvg = [aAvg; mean(aRel(poses))];
+    xAvg = [xAvg; mean(xAll(poses))];
+    yAvg = [yAvg; mean(yAll(poses))];
+    if length(poses) > 0.95*max(numObs)
+        xAllNorm = [xAllNorm; xAll(poses)-mean(xAll(poses))];
+        yAllNorm = [yAllNorm; yAll(poses)-mean(yAll(poses))];
+        fAllNorm = [fAllNorm; fAll(poses)];
+    end
+end
+dx = polyfit(fAllNorm*step,xAllNorm,5); % ... x vs z, defined as frame*z-step
+dy = polyfit(fAllNorm*step,yAllNorm,5); % ... y vs z
 dz = polyfit(aAll,fAll*step,5); % ... z vs angle
 
 dd = polyfit(aAll,distAll,8); % ... distance vs angle
